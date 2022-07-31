609b3f797e7e41f307f20c17651eb5b00b720f9e
Noah Reece <noah.w.reece@gmail.com>
2022-07-31 16:33:28 -0400

    Refactored Loading Screen to remove font loading

--- src/screens/LoadingScreen.js
+++ src/screens/LoadingScreen.js
@@ -1,16 +1,11 @@
 import { styles } from "../theme/styles";
 import fonts from "../theme/fonts";
 import { Text, ActivityIndicator } from "react-native";
-// import { useFonts } from "expo-font";
 import CenteredContainer from "../containers/CenteredContainer";
 
 // TODO: Should Implement Title component, rather than Text
 
 const LoadingScreen = (props) => {
-  // let [fontsLoaded] = useFonts({
-  //   AppMed: require("../assets/fonts/AppareoMedium.ttf"),
-  // });
-
   return (
     <CenteredContainer>
       <Text style={{ fontFamily: fonts.type.titleDefault }}>WORDERBY</Text>
