760c378278091c0a07f9393dbb2543fbbaec2e0f
Noah Reece <noah.w.reece@gmail.com>
2022-07-31 17:14:14 -0400

    Splash displays for 2 seconds before disappearing

--- App.js
+++ App.js
@@ -1,39 +1,59 @@
 import { styles } from "./src/theme/styles";
 import * as utils from "./src/utils/utils";
-import { useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { View, StatusBar, Platform } from "react-native";
-import AppLoading from "expo-app-loading";
+// import AppLoading from "expo-app-loading";
+import * as SplashScreen from "expo-splash-screen";
 import useFonts from "./src/hooks/useFonts";
 import BackgroundGradientContainer from "./src/containers/BackgroundGradientContainer";
 import LoadingScreen from "./src/screens/LoadingScreen";
 
 // TODO: explore possibly utilizing Status Bar from "expo-status-bar"
 
+// keep splash screen visible until prompted
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
-  const [isReady, setIsReady] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
 
-  const loadFonts = async () => {
-    await useFonts();
-  };
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        // do platform configuration
+        utils.configurePlatformSpecificSettings();
 
-  // do platform configuration
-  utils.configurePlatformSpecificSettings();
+        // load fonts
+        await useFonts();
 
-  if (!isReady) {
-    return (
-      <AppLoading
-        startAsync={loadFonts}
-        onFinish={() => setIsReady(true)}
-        onError={() => {
-          console.log("App Loading errored out!");
-        }}
-      />
-    );
+        // TODO: remove below
+        // artificial 2 second delay after loading
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setIsLoaded(true);
+      }
+    };
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (isLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isLoaded]);
+
+  // render only splash screen if app not loaded
+  if (!isLoaded) {
+    return null;
   }
 
   return (
-    <BackgroundGradientContainer>
-      <LoadingScreen />
-    </BackgroundGradientContainer>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <BackgroundGradientContainer>
+        <LoadingScreen />
+      </BackgroundGradientContainer>
+    </View>
   );
 }
