d510a609660754303ada8f406585452bf0ce8cff
Noah Reece <noah.w.reece@gmail.com>
2022-08-01 19:18:39 -0400

    minor refactoring

--- src/hooks/useGoogleAuth.js
+++ src/hooks/useGoogleAuth.js
@@ -20,5 +20,5 @@ export default useGoogleAuth = () => {
     }
   }, [response]);
 
-  return { accessToken, promptAsync };
+  return { googleToken: accessToken, getGoogleToken: promptAsync };
 };
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,23 +1,23 @@
-import { useEffect, useState } from "react";
 import { styles } from "../theme/styles";
 import fonts from "../theme/fonts";
+import { useEffect, useState } from "react";
 import { Text, Image, Button, View } from "react-native";
-import CenteredContainer from "../containers/CenteredContainer";
-import useGoogleAuth from "../hooks/useGoogleAuth";
 import * as WebBrowser from "expo-web-browser";
+import useGoogleAuth from "../hooks/useGoogleAuth";
+import CenteredContainer from "../containers/CenteredContainer";
 
 // allows auth session to complete and return results
 WebBrowser.maybeCompleteAuthSession();
 
 const LoginScreen = (props) => {
   const [userInfo, setUserInfo] = useState();
-  const { accessToken, promptAsync } = useGoogleAuth();
+  const { googleToken, getGoogleToken } = useGoogleAuth();
 
   const getUserData = async () => {
     let userInfoResponse = await fetch(
       "https://www.googleapis.com/userinfo/v2/me",
       {
-        headers: { Authorization: `Bearer ${accessToken}` },
+        headers: { Authorization: `Bearer ${googleToken}` },
       }
     );
 
@@ -28,6 +28,7 @@ const LoginScreen = (props) => {
 
   const showUserInfo = () => {
     if (userInfo) {
+      console.log(userInfo);
       return (
         <View style={styles.centered}>
           <Image source={{ uri: userInfo.picture }} style={styles.profilePic} />
@@ -42,9 +43,11 @@ const LoginScreen = (props) => {
     <CenteredContainer>
       {showUserInfo()}
       <Button
-        title={accessToken ? "Get User Data" : "Login"}
+        title={googleToken ? "Get User Data" : "Login"}
         onPress={
-          accessToken ? getUserData : () => promptAsync({ showInRecents: true })
+          googleToken
+            ? getUserData
+            : () => getGoogleToken({ showInRecents: true })
         }
       />
     </CenteredContainer>
