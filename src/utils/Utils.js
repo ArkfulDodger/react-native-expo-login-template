37026a5edbbd8a32d40ed7bfc88a586adebbe181
Noah Reece <noah.w.reece@gmail.com>
2022-08-06 17:13:36 -0400

    added email validation from emailregex.com

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -10,11 +10,13 @@ export const configurePlatformSpecificSettings = () => {
   }
 };
 
+// regex for email validation sourced from http://emailregex.com/
+const emailRegex =
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
 // returns whether the provided string is a valid email address
 export const isValidEmail = (str) => {
-  // TODO: write email validation
-  console.log("auto-approved email validation");
-  return true;
+  return emailRegex.test(str);
 };
 
 // will toggle the provided (bool) state ONLY if all of its conditions indicate it should change
