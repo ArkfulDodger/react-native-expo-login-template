3ecffbbdd3dcad68de6792a37b199d297da81a6d
Noah Reece <noah.w.reece@gmail.com>
2022-08-22 23:57:28 -0400

    organized styles file

--- src/theme/styles.js
+++ src/theme/styles.js
@@ -1,66 +1,89 @@
-import { StyleSheet, Platform } from "react-native";
+import { StyleSheet } from "react-native";
 import useFadeSlide from "../hooks/useFadeSlide";
 import fonts from "./fonts";
 
 const forSlide = useFadeSlide();
 
 export const styles = StyleSheet.create({
+  //#region CONTAINERS --------------------------------------------------------
+
   // fill the parent container
   fillContainer: {
     flex: 1,
   },
 
-  pageMargins: {
-    marginHorizontal: 15,
-    marginVertical: 15,
-  },
-
-  spreadColumn: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-
-  centeredColumn: {
-    flexDirection: "column",
-    alignItems: "center",
-    alignSelf: "stretch",
-  },
-
-  spreadRow: {
-    alignSelf: "stretch",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-
-  spacedRow: {
-    alignSelf: "stretch",
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
-  },
-
-  centeredRow: {
-    alignSelf: "center",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-
-  row: {
-    alignItems: "center",
-    flexDirection: "row",
-  },
-
-  // center on both axes
+  // container with items centered on both axes
   centered: {
     alignItems: "center",
     justifyContent: "center",
   },
 
-  // profile pic size for demo purposes
+  // container with items centered in a column of evenly spaced items
+  spreadColumn: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+
+  // container of parent's width with items in a centered column
+  centeredColumn: {
+    alignSelf: "stretch",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+
+  // container with items centered in a row of evenly spaced items
+  spreadRow: {
+    alignSelf: "stretch",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+
+  // container with items centered in a row of evenly spaced items (space around)
+  spacedRow: {
+    alignSelf: "stretch",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+
+  // container of parent's width with items centered vertically in an evenly spaced row
+  centeredRow: {
+    alignSelf: "center",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+
+  // container with items centered verically in a row
+  row: {
+    alignItems: "center",
+    flexDirection: "row",
+  },
+
+  //#endregion
+
+  //#region LAYOUT ------------------------------------------------------------
+
+  // standard margin to pad from screen edges to content
+  pageMargins: {
+    marginHorizontal: 15,
+    marginVertical: 15,
+  },
+
+  // remove padding and margins
+  noPaddingNoMargin: {
+    padding: 0,
+    margin: 0,
+  },
+
+  //#endregion
+
+  //#region IMAGES ------------------------------------------------------------
+
+  // profile pic size
   profilePic: {
     width: 50,
     height: 50,
@@ -71,13 +94,27 @@ export const styles = StyleSheet.create({
     marginVertical: 40,
   },
 
+  buttonAvatarImage: {
+    borderRadius: 0,
+    margin: 5,
+    backgroundColor: "transparent",
+  },
+
+  //#endregion
+
+  //#region INPUTS ------------------------------------------------------------
+
   // auth page text input styling
   authInput: {
     alignSelf: "stretch",
     fontSize: fonts.size.font20,
   },
 
-  // auth page buttons
+  //#endregion
+
+  //#region BUTTONS -----------------------------------------------------------
+
+  // primary auth page buttons
   authButton: {
     alignSelf: "stretch",
     padding: 3,
@@ -85,14 +122,14 @@ export const styles = StyleSheet.create({
     elevation: 6,
   },
 
-  // auth page buttons
+  // primary auth page buttons (when disabled)
   authButtonDisabled: {
     alignSelf: "stretch",
     padding: 3,
     marginVertical: 10,
   },
 
-  // auth page button text
+  // the text for primary auth page buttons
   authButtonText: {
     fontSize: fonts.size.font24,
     fontFamily: fonts.type.defaultLight,
@@ -100,22 +137,20 @@ export const styles = StyleSheet.create({
     marginHorizontal: 16,
   },
 
-  // auth page buttons
+  // auth page text-only buttons
   textButton: {
     padding: 5,
   },
 
-  // text page button text
+  // the text for auth page text-only buttons
   textButtonText: {
     fontSize: fonts.size.font16,
     fontFamily: fonts.type.defaultLight,
   },
 
-  // remove padding and margins
-  noPaddingNoMargin: {
-    padding: 0,
-    margin: 0,
-  },
+  //#endregion
+
+  //#region NAVIGATION --------------------------------------------------------
 
   defaultStackOptions: {
     headerShown: false,
@@ -123,9 +158,5 @@ export const styles = StyleSheet.create({
     presentation: "modal",
   },
 
-  buttonAvatarImage: {
-    borderRadius: 0,
-    margin: 5,
-    backgroundColor: "transparent",
-  },
+  //#endregion
 });
